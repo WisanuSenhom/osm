49ef64926fbf5724036048e9e4d2b3aa2f2fdc10
Wisanu Senhom <48409780+WisanuSenhom@users.noreply.github.com>
2023-09-19 21:46:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -17,17 +17,17 @@ async function getProfile() {
         output += `
         <div class="col">
         <div class="card text-center border-0">
-                  <h5 class="card-header">
+                  <h2 class="card-header">
             ${user.name} 
-             </h5>
-             <ul class="list-group list-group-flush">
-          อายุ ${user.age}
+             </h2>
+             <ul class="list-group list-group-flush"><h3>
+          อายุ ${user.age}</h3>
           </ul>
           <ul class="list-group list-group-flush">
-            <a href="${user.ncd}" class="list-group-item list-group-item-action list-group-item-primary" type="button">${user.linktxt}</a>          
-           <a href="${user.adl}" class="list-group-item list-group-item-action list-group-item-success" type="button">ประเมินความสามารถในการทำกิจวัตรประจำวัน ADL</a>        
-          <a href="${user.qq}" class="list-group-item list-group-item-action list-group-item-warning" type="button">ประเมินโรคซึมเศร้า 2Q,9Q</a>    
-         <a href="${user.ncdplus}" class="list-group-item list-group-item-action list-group-item-info" type="button">ประเมินภาวะสุขภาพกาย สุขภาพจิต</a>
+            <a href="${user.ncd}" class="list-group-item list-group-item-action list-group-item-primary" type="button"><h2>${user.linktxt}</h2></a>          
+           <a href="${user.adl}" class="list-group-item list-group-item-action list-group-item-success" type="button"><h2>ประเมินความสามารถในการทำกิจวัตรประจำวัน ADL</h2></a>        
+          <a href="${user.qq}" class="list-group-item list-group-item-action list-group-item-warning" type="button"><h2>ประเมินโรคซึมเศร้า 2Q,9Q</h2></a>    
+         <a href="${user.ncdplus}" class="list-group-item list-group-item-action list-group-item-info" type="button"><h2>ประเมินภาวะสุขภาพกาย สุขภาพจิต</h2></a>
         </ul>
    
         </div>    
